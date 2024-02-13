f10c3ce17f3b21f8cf7189037b362854088b7d5b
Annateresa01 <shajianna013@gmail.com>
2024-02-14 01:03:20 +0530

    ok

--- controllers/addsecrouter.js
+++ controllers/addsecrouter.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const securitymodel=require("../models/securitymodel")
+const bcrypt=require("bcrypt")
 
 const router=express.Router()
 
@@ -12,4 +13,47 @@ router.post("/add",async(req,res)=>{
     })
 
 })
+router.get("/view",async(req,res)=>{
+
+    let data=await securitymodel.find()
+    res.json(data)
+})
+
+
+
+router.post("/signin",async(req,res)=>{
+    let input=req.body
+    let emailId=req.body.emailId
+    let data=await securitymodel.findOne({"emailId":emailId})
+    if(!data){
+        return res.json(
+            {
+                status:"invalid emailid"
+            }
+        )
+    }
+    console.log(data)
+    let dbpassword=data.password
+    let inputpassword=req.body.password
+    console.log(dbpassword)
+    console.log(inputpassword)
+    //const match=await bcrypt.//compare(inputpassword,dbpassword)
+    if(!(dbpassword==inputpassword))
+    {
+     return res.json(
+        {
+            status:"Incorrect"
+        }
+    )
+    }
+    else{
+    res.json(
+        {
+            status:"Success","userdata":data
+        }
+    )
+    }
+    
+    
+    })
 module.exports=router
